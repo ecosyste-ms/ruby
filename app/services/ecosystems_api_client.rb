46eedec67d1a0929bba4a8d4714131a36d2bb664
Andrew Nesbitt <andrewnez@gmail.com>
2025-09-22 12:45:37 +0100

    Add X-API-Key header when making requests to ecosyste.ms apis

--- app/services/ecosystems_api_client.rb
+++ app/services/ecosystems_api_client.rb
@@ -2,6 +2,7 @@ class EcosystemsApiClient
   def self.connection(url)
     Faraday.new(url: url) do |faraday|
       faraday.headers['User-Agent'] = 'ruby.ecosyste.ms'
+      faraday.headers['X-API-Key'] = ENV['ECOSYSTEMS_API_KEY'] if ENV['ECOSYSTEMS_API_KEY']
       faraday.response :follow_redirects
       faraday.adapter Faraday.default_adapter
     end
